e5997c6aa1909a76242175c96d3aebe207997a7a
mpokidov <m.pokidov@getmail.cc>
2020-07-12 14:11:20 +0300

    first

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,82 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/users">Users</router-link> |
-      <router-link to="/UserEdit">Users</router-link> |
-      <router-link to="/newUser">New User</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <tree-item
+    class="item"
+    :item="treeData"
+    @make-folder="makeFolder"
+    @add-item="addItem"
+  ></tree-item>
 </template>
 
-<style lang="less">
-  * {
-    box-sizing: border-box;
+<script>
+import tree-item from '@/components/tree-item.vue'
+const treeData = {
+  name: 'root',
+  selected: true,
+  children: [
+    {
+      name: 'item1',
+      selected: true
+    },
+    {
+      name: 'wat',
+      selected: true
+    },
+    {
+      name: 'item2',
+      children: [
+        {
+          name: 'item3',
+          selected: true,
+          children: [{ name: 'item4' }, { name: 'item5' }]
+        },
+        {
+          name: 'item6',
+          selected: true
+        },
+        { name: 'item7' },
+        {
+          name: 'item8',
+          selected: true,
+          children: [
+            {
+              name: 'item9',
+              children: [
+                {
+                  name: 'item10',
+                  selected: true,
+                  children: [{ name: 'item11' }, { name: 'item12' }]
+                },
+                { name: 'item13', selected: true }
+              ]
+            },
+            { name: 'item14', selected: true }
+          ]
+        }
+      ]
+    }
+  ]
+}
+
+export default {
+  name: 'App',
+  components: {
+    TreeList
+  },
+  data: function () {
+    return {
+      treeState: treeData
+    }
   }
+}
+</script>
+
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-ul,li,ol {
-  list-style: none;
-}
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  margin-top: 60px;
 }
 </style>
